89dd5256f65a1d3acbe3e13ddd6c0c180f08cb5e
Lorenz Gerber <lorenz.gerber@lablicate.com>
2016-11-12 10:29:57 +0100

    setup in eclipse IDE

--- src/Test1.java
+++ src/Test1.java
@@ -2,43 +2,43 @@
  * Created by loge on 2016-11-10.
  */
 public class Test1 implements TestClass {
-    private TestClass testClass;
+    private MyInt myInt;
 
     public Test1() {
     }
 
     public void setUp() {
-        testClass =new TestClass();
+        myInt =new MyInt();
     }
 
     public void tearDown() {
-        testClass =null;
+        myInt =null;
     }
 
     //Test that should succeed
     public boolean testInitialisation() {
-        return testClass.value()==0;
+        return myInt.value()==0;
     }
 
     //Test that should succeed
     public boolean testIncrement() {
-        testClass.increment();
-        testClass.increment();
-        return testClass.value()==2;
+        myInt.increment();
+        myInt.increment();
+        return myInt.value()==2;
 
     }
 
     //Test that should succeed
     public boolean testDecrement() {
-        testClass.increment();
-        testClass.decrement();
-        return testClass.value()==0;
+        myInt.increment();
+        myInt.decrement();
+        return myInt.value()==0;
     }
 
     //Test that should fail
     public boolean testFailingByException() {
-        testClass =null;
-        testClass.decrement();
+        myInt =null;
+        myInt.decrement();
         return true;
 
     }
