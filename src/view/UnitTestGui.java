7308c59fca590c5b53278405cf9f16925c4deeda
Lorenz Gerber <lorenz.gerber@lablicate.com>
2016-11-14 22:30:47 +0100

    working on javadoc

--- src/view/UnitTestGui.java
+++ src/view/UnitTestGui.java
@@ -1,10 +1,32 @@
+/*
+ * 
+ * This file is part of UnitTest.
+ *
+ * UnitTest is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * UnitTest is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with UnitTest.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
 package view;
 
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 
 import javax.swing.*;
-
+/**
+ * 
+ * @author lgerber
+ *
+ */
 public class UnitTestGui {
 
     private static final int FRAME_WIDTH = 400;
@@ -55,8 +77,10 @@ public class UnitTestGui {
 	consoleOutput.setLineWrap(true);
 	consoleOutput.setEditable(false);
 	scrollPane = new JScrollPane(consoleOutput);
-	scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
-	scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
+	scrollPane.setHorizontalScrollBarPolicy(
+		ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+	scrollPane.setVerticalScrollBarPolicy(
+		ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
 	middlePanel.add(scrollPane);
 
 	return middlePanel;
