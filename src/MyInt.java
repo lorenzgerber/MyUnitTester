7684cc68d51aea25479e1a818eafbc2aefb86caa
Lorenz Gerber <lorenz.gerber@lablicate.com>
2016-11-14 22:20:19 +0100

    working on docu

--- src/MyInt.java
+++ src/MyInt.java
@@ -1,6 +1,3 @@
-/**
- * Created by loge on 2016-11-10.
- */
 public class MyInt {
     private int val;
 
--- src/UnitTestMain.java
+++ src/UnitTestMain.java
@@ -1,8 +1,35 @@
-import javax.swing.SwingUtilities;
+/*
+ * 
+ * This file is part of UnitTest.
+ *
+ * UnitTest is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
 
+ * UnitTest is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with UnitTest.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+import javax.swing.SwingUtilities;
 import controller.UnitTestController;
 import view.UnitTestGui;
 
+/**
+ * UnitTestMain
+ * 
+ * This file contains a main method to run the java class 
+ * UnitTest with a graphical user interface. 
+ * 
+ * @author Lorenz Gerber
+ * @version 1.0
+ *
+ */
 public class UnitTestMain {
 
     public static void main(String args[]) {
